57c4757173f9938584cd6c9420ae85ac4d92e201
Mobina Naghizadeh <123436283+mobinangz@users.noreply.github.com>
2023-02-08 17:03:58 +0330

    second try

--- ElectricalCapacitorLifeCalculator.php
+++ ElectricalCapacitorLifeCalculator.php
@@ -1,13 +1,25 @@
 <?php
-$L1=1000;
-$VR=2;
-$V0=10;
-$Tm=6;
-$TA=3;
+$L1=readline('Enter L1  ');
+$Vr=readline('Enter Vr  ');
+if($Vr<=2){$Vr=$Vr;}
+else if($Vr>2){
+	while($Vr>2) {
+	echo("Vr should be less or equal to 2 " . "\n");
+	$Vr=readline('Enter Vr  ');
+}}
+$Vo=readline('Enter Vo  ');
+$Tm=readline('Enter Tm  ');
+$TA=readline('Enter TA  ');
 //$DealtaT=($TA-$Tm);
 $x=($Tm-($TA))/10;
-
-$sum=$L1*($VR/$V0)*2**$x;
+$sum=$L1*($Vr/$Vo)*2**$x;
 echo $sum;
+echo "\n";
+
+
+
+
+
+
 
 
